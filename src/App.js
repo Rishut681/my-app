67497418513ae95a96958cbee4c6d9bc1f4bb42d
Rishu Raj <131179273+Rishut681@users.noreply.github.com>
2023-07-15 19:59:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import './App.css';
 function App() {
   return (
     <>
-    
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Active</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
     </>
-   );
+  );
 }
 
 export default App;
