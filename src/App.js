36f40a646a701b7b438ac4b936bbc615d5f4b72e
Rishu Raj <131179273+Rishut681@users.noreply.github.com>
2023-07-18 20:56:26 +0530

    start

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import './App.css';
 function App() {
   return (
     <>
-      <ul className="nav justify-content-center">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Active</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
+      <div className='body'>
+        <div className="header">
+          <div className="logo">
+            <img src="https://i.pinimg.com/originals/12/34/f5/1234f5670f0ba42a6b735d9c568566b3.jpg" alt="..." />
+          </div>
+          <div>
+            <nav className="navbar">
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/" target="_blank">Driving Schools</a></li>
+                <li><a href="/">Services</a></li>
+                <li><a href="http://127.0.0.1:5500/contact.html?fname=mwkm&lname=wijssi&email=xnhwbbywb%40gmail.com&sub=nswi&message=sjqwushu"
+                  target="_blank">Contact us</a></li>
+                <li><a href="/">Review</a></li>
+                <li><a href="/">Carrier</a></li>
+                <li>
+                <div className="right">
+                  <p>Dial for any Query </p>
+                  <p>xx-xxxx88</p>
+                </div>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
