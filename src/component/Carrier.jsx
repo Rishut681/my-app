012d32425399610f0a5965d42b6279b458ac52d2
Rishu Raj <131179273+Rishut681@users.noreply.github.com>
2023-07-28 19:24:04 +0530

    bt

--- src/component/Carrier.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Carrier() {
-  return (
-    <div>Carrier</div>
-  )
-}
--- src/component/Driving-school.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Driving-school() {
-  return (
-    <div>Driving-school</div>
-  )
-}
